2383d56b60cf5d5e9da2ac30c607ebd0a390c98c
N74Mmerino <nnicolasmmerino@gmail.com>
2024-10-04 20:51:35 -0300

    primer commit

---
+++ api/estacionamiennto/agregar.php
@@ -0,0 +1,18 @@
+<?php
+
+header('Content-Type: application/json');
+
+require_once 'modelosRespuestas/agregarRespuesta.php';
+require_once 'modelosRequest/agregarRequest.php';
+
+//se obtiene el body
+$json = file_get_contents('php://input',true);
+// Convertir el body en un objeto
+$req = json_decode($json);
+
+$resp= new AgregarRespuesta();
+
+
+
+
+echo json_encode ($resp);
